46a73cb353e116b92261b116cc0a63efb968b3a6
mariotejeda2001 <marioe_tl19@hotmail.com>
2026-04-15 13:04:44 -0700

    implementaciones para hacer pruebas de GET POST

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from entities.trip import Trip
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return jsonify({"succese": True, "Message":"Hello world"}), 200
+    return jsonify ({"success":True, "message":"Hello World"}),200
 
-if __name__ == "__name__":
+@app.route('/trips', methods=['GET'])
+def trips():
+    trips = Trip.get_all()
+    return trips
+
+@app.route('/trip', methods=['POST'])
+def save_trip():
+    data= request.json
+    trip = Trip(name=data['name'],
+                city=data['city'],
+                latitude=data['latitude'],
+                longitude=data['longitude'])
+    id = trip.save()
+    success = id is not None
+    return jsonify({"success": success, "id": id}), 200 if success else 500
+
+if __name__ == '__main__':
     app.run(port=5001)
